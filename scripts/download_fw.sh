alessandroaraujo5/ExtremeROM-Revived

commit dd08b6129e16ac9db1aad8d31cfe31c26ca935c3

scripts: require buildenv.sh to be run, clean up
Change-Id: I68eebf1d0b0f42b3687df8feb8455f46c6dbfef4

scripts/download_fw.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Copyright (C) 2023 BlackMesa123
 #
@@ -16,18 +16,11 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# shellcheck disable=SC1091,SC2069
+# shellcheck disable=SC1091
 
 set -e
 
 # [
-SRC_DIR="$(git rev-parse --show-toplevel)"
-OUT_DIR="$SRC_DIR/out"
-ODIN_DIR="$OUT_DIR/odin"
-TOOLS_DIR="$OUT_DIR/tools/bin"
-
-PATH="$TOOLS_DIR:$PATH"
-
 GET_LATEST_FIRMWARE()
 {
     curl -s --retry 5 --retry-delay 5 "https://fota-cloud-dn.ospserver.net/firmware/$REGION/$MODEL/version.xml" \
@@ -40,7 +33,7 @@ DOWNLOAD_FIRMWARE()
     PDR="$(pwd)"
 
     cd "$ODIN_DIR"
-    samfirm -m "$MODEL" -r "$REGION" 2>&1 > /dev/null \
+    { samfirm -m "$MODEL" -r "$REGION" > /dev/null; } 2>&1 \
         && touch "$ODIN_DIR/${MODEL}_${REGION}/.downloaded" \
         || exit 1
     [ -f "$ODIN_DIR/${MODEL}_${REGION}/.downloaded" ] && {
@@ -52,10 +45,6 @@ DOWNLOAD_FIRMWARE()
     echo ""
     cd "$PDR"
 }
-
-FORCE=false
-
-source "$OUT_DIR/config.sh"
 
 FIRMWARES=( "$SOURCE_FIRMWARE" "$TARGET_FIRMWARE" )
 if [ "${#SOURCE_EXTRA_FIRMWARES[@]}" -ge 1 ]; then
@@ -71,6 +60,8 @@ if [ "${#TARGET_EXTRA_FIRMWARES[@]}" -ge 1 ]; then
     done
 fi
 # ]
+
+FORCE=false
 
 while [ "$#" != 0 ]; do
     case "$1" in

scripts/extract_fw.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Copyright (C) 2023 BlackMesa123
 #
@@ -16,19 +16,11 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# shellcheck disable=SC1091,SC2005,SC2069
+# shellcheck disable=SC1091
 
 set -e
 
 # [
-SRC_DIR="$(git rev-parse --show-toplevel)"
-OUT_DIR="$SRC_DIR/out"
-ODIN_DIR="$OUT_DIR/odin"
-FW_DIR="$OUT_DIR/fw"
-TOOLS_DIR="$OUT_DIR/tools/bin"
-
-PATH="$TOOLS_DIR:$PATH"
-
 GET_IMG_FS_TYPE()
 {
     if [[ "$(xxd -p -l "2" --skip "1080" "$1")" == "53ef" ]]; then
@@ -86,7 +78,7 @@ EXTRACT_OS_PARTITIONS()
             tar xf "$AP_TAR" "super.img.lz4"
             lz4 -d -q --rm "super.img.lz4" "super.img.sparse"
             simg2img "super.img.sparse" "super.img" && rm "super.img.sparse"
-            lpunpack "super.img" 2>&1 > /dev/null
+            { lpunpack "super.img" > /dev/null; } 2>&1
             lpdump "super.img" > "lpdump" && rm "super.img"
         fi
 
@@ -103,7 +95,7 @@ EXTRACT_OS_PARTITIONS()
                     PREFIX=""
                     [ -d "$PARTITION" ] && rm -rf "$PARTITION"
                     mkdir -p "$PARTITION"
-                    fuse.erofs "$img" "tmp_out" 2>&1 > /dev/null
+                    { fuse.erofs "$img" "tmp_out" > /dev/null; } 2>&1
                     cp -a --preserve=all tmp_out/* "$PARTITION"
                     ;;
                 "f2fs" | "ext4")
@@ -141,7 +133,7 @@ EXTRACT_OS_PARTITIONS()
                         CAPABILITIES="0x0"
                         ;;
                 esac
-                echo "$($PREFIX stat -c "%n %u %g %a capabilities=$CAPABILITIES" "$i")" >> "fs_config-$PARTITION"
+                $PREFIX stat -c "%n %u %g %a capabilities=$CAPABILITIES" "$i" >> "fs_config-$PARTITION"
             done <<< "$($PREFIX find "tmp_out")"
             if [ "$PARTITION" = "system" ]; then
                 sed -i "s/tmp_out /\/ /g" "file_context-$PARTITION" \
@@ -203,10 +195,6 @@ EXTRACT_ALL()
     echo ""
 }
 
-FORCE=false
-
-source "$OUT_DIR/config.sh"
-
 FIRMWARES=( "$SOURCE_FIRMWARE" "$TARGET_FIRMWARE" )
 if [ "${#SOURCE_EXTRA_FIRMWARES[@]}" -ge 1 ]; then
     for i in "${SOURCE_EXTRA_FIRMWARES[@]}"
@@ -221,6 +209,8 @@ if [ "${#TARGET_EXTRA_FIRMWARES[@]}" -ge 1 ]; then
     done
 fi
 # ]
+
+FORCE=false
 
 while [ "$#" != 0 ]; do
     case "$1" in

scripts/internal/gen_config_file.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Copyright (C) 2023 BlackMesa123
 #
@@ -18,13 +18,10 @@
 
 # shellcheck disable=SC1090,SC1091
 
-set -Eeu
+set -Ee
 
 # [
-SRC_DIR="$(git rev-parse --show-toplevel)"
-OUT_DIR="$SRC_DIR/out"
-
-trap "rm -f $OUT_DIR/config.sh" ERR
+trap 'rm -f $OUT_DIR/config.sh' ERR
 
 GET_OFFICIAL_STATUS()
 {

buildenv.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Copyright (C) 2023 BlackMesa123
 #
@@ -16,14 +16,21 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# shellcheck disable=SC2012
+# shellcheck disable=SC1091,SC2012,SC2034
+
+set -a
 
 # [
 SRC_DIR="$(git rev-parse --show-toplevel)"
 OUT_DIR="$SRC_DIR/out"
+TMP_DIR="$OUT_DIR/tmp"
+ODIN_DIR="$OUT_DIR/odin"
+FW_DIR="$OUT_DIR/fw"
+APKTOOL_DIR="$OUT_DIR/apktool"
+WORK_DIR="$OUT_DIR/work_dir"
+TOOLS_DIR="$OUT_DIR/tools/bin"
 
-s='\<'
-e='\>'
+PATH="$TOOLS_DIR:$PATH"
 
 run_cmd()
 {
@@ -34,7 +41,7 @@ run_cmd()
     if [ -z "$CMD" ] || [ "$CMD" = "-h" ]; then
         echo -e "Available cmds:\n$CMDS"
         return 1
-    elif [[ ! "$CMDS" =~ $s$CMD$e ]]; then
+    elif ! echo "$CMDS" | grep -q -w "$CMD"; then
         echo "\"$CMD\" is not valid."
         echo -e "Available cmds:\n$CMDS"
         return 1
@@ -51,7 +58,7 @@ if [ "$#" != 1 ]; then
     echo "Usage: source buildenv.sh <target>"
     echo -e "Available devices:\n$TARGETS"
     return 1
-elif [[ ! "$TARGETS" =~ $s$1$e ]]; then
+elif ! echo "$TARGETS" | grep -q -w "$1"; then
     echo "\"$1\" is not valid target."
     echo -e "Available devices:\n$TARGETS"
     return 1
@@ -59,6 +66,13 @@ else
     mkdir -p "$OUT_DIR"
     run_cmd build_dependencies
     bash "$SRC_DIR/scripts/internal/gen_config_file.sh" "$1" || return 1
+    source "$OUT_DIR/config.sh"
+
+    echo "=============================="
+    sed "/Automatically/d" "$OUT_DIR/config.sh"
+    echo "=============================="
 fi
+
+unset TARGETS
 
 return 0

scripts/apktool.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Copyright (C) 2023 BlackMesa123
 #
@@ -18,17 +18,9 @@
 
 # shellcheck disable=SC1091
 
-set -e
+set -eu
 
 # [
-SRC_DIR="$(git rev-parse --show-toplevel)"
-OUT_DIR="$SRC_DIR/out"
-APKTOOL_DIR="$OUT_DIR/apktool"
-WORK_DIR="$OUT_DIR/work_dir"
-TOOLS_DIR="$OUT_DIR/tools/bin"
-
-PATH="$TOOLS_DIR:$PATH"
-
 PRINT_USAGE()
 {
     echo "Usage: apktool d[ecode]/b[uild] <apk> (<apk>...)"
@@ -105,11 +97,11 @@ DO_DECOMPILE()
     fi
 
     if [[ "$APK_PATH" == *".jar" ]]; then
-        API="-api $SOURCE_API_LEVEL"
+        API=( "-api" "$SOURCE_API_LEVEL" )
     fi
 
     echo "Decompiling $OUT_DIR"
-    apktool -q d $API -b $FORCE -o "$APKTOOL_DIR$OUT_DIR" -p "$FRAMEWORK_DIR" "$APK_PATH"
+    apktool -q d "${API[@]}" -b $FORCE -o "$APKTOOL_DIR$OUT_DIR" -p "$FRAMEWORK_DIR" "$APK_PATH"
     sed -i "s/classes.dex/dex/g" "$APKTOOL_DIR$OUT_DIR/apktool.yml"
 
     # Workaround for U framework.jar
@@ -198,8 +190,6 @@ DO_RECOMPILE()
     fi
 }
 
-source "$OUT_DIR/config.sh"
-
 FRAMEWORK_DIR="$APKTOOL_DIR/bin/fw"
 # ]
 
@@ -235,6 +225,8 @@ case "$1" in
 esac
 
 shift
+
+FORCE=""
 
 if [[ "$1" == "-f" ]]|| [[ "$1" == "--force" ]]; then
     FORCE="-f"

scripts/build_dependencies.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Copyright (C) 2023 BlackMesa123
 #
@@ -16,12 +16,10 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-set -e
+set -eu
 
 # [
-SRC_DIR="$(git rev-parse --show-toplevel)"
-OUT_DIR="$SRC_DIR/out"
-TOOLS_DIR="$OUT_DIR/tools/bin"
+JOBS="$(nproc)"
 
 CHECK_TOOLS()
 {
@@ -35,8 +33,6 @@ CHECK_TOOLS()
 
     $EXISTS
 }
-
-JOBS="$(nproc)"
 
 BUILD_ANDROID_TOOLS()
 {

scripts/build_fs_image.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Copyright (C) 2023 BlackMesa123
 #
@@ -16,17 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# shellcheck disable=SC2069
-
-set -e
-
-# [
-SRC_DIR="$(git rev-parse --show-toplevel)"
-OUT_DIR="$SRC_DIR/out"
-TOOLS_DIR="$OUT_DIR/tools/bin"
-
-PATH="$TOOLS_DIR:$PATH"
-# ]
+set -eu
 
 if [ "$#" != 4 ]; then
     echo "Usage: build_fs_image <fs> <dir> <file_context> <fs_config>"
@@ -100,9 +90,9 @@ if $EXT4; then
     fi
 
     $SPARSE && SPARSE_FLAG="-s"
-    mkuserimg_mke2fs $SPARSE_FLAG -j 0 -T 1230735600 -C "$4" \
+    { mkuserimg_mke2fs $SPARSE_FLAG -j 0 -T 1230735600 -C "$4" \
         -L "$MOUNT_POINT" -i "$(echo "$INODES + $SPARE_INODES" | bc -l)" -I 256 \
-        "$2" "$2/../$PARTITION.img" ext4 "$MOUNT_POINT" "$IMG_SIZE" "$3" 2>&1 > /dev/null
+        "$2" "$2/../$PARTITION.img" ext4 "$MOUNT_POINT" "$IMG_SIZE" "$3" > /dev/null; } 2>&1
 elif $F2FS; then
     [[ $PARTITION == "system" ]] && MOUNT_POINT="/" || MOUNT_POINT="$PARTITION"
     IMG_SIZE=$(du -sb "$2" | cut -f 1)

scripts/cleanup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Copyright (C) 2023 BlackMesa123
 #
@@ -16,17 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# shellcheck disable=SC2069,SC2164
-
-# [
-SRC_DIR="$(git rev-parse --show-toplevel)"
-OUT_DIR="$SRC_DIR/out"
-ODIN_DIR="$OUT_DIR/odin"
-FW_DIR="$OUT_DIR/fw"
-APKTOOL_DIR="$OUT_DIR/apktool"
-WORK_DIR="$OUT_DIR/work_dir"
-TOOLS_DIR="$OUT_DIR/tools/bin"
-# ]
+set -eu
 
 ALL=false
 ODIN=false
@@ -112,7 +102,7 @@ if $TOOLS; then
         cd "$SRC_DIR/external/erofs-utils" && git clean -f -d -x && cd -
         cd "$SRC_DIR/external/samfirm.js" && git clean -f -d -x && cd -
         cd "$SRC_DIR/external/smali" && git clean -f -d -x && cd -
-    } 2>&1 > /dev/null
+    } > /dev/null
 fi
 
 exit 0

commit 60ca5bbee6bd4ff1d7243f11dffc09ea3343df20

scripts/*_fw: fix regression caused by dd08b6129e16ac9db1aad8d31cfe31c26ca935c3
Change-Id: Ica5f94cef68b06b3456710a3ee75fcc4742b8f71

scripts/download_fw.sh
@@ -47,12 +47,14 @@ DOWNLOAD_FIRMWARE()
 }
 
 FIRMWARES=( "$SOURCE_FIRMWARE" "$TARGET_FIRMWARE" )
+IFS=':' read -a SOURCE_EXTRA_FIRMWARES <<< "$SOURCE_EXTRA_FIRMWARES"
 if [ "${#SOURCE_EXTRA_FIRMWARES[@]}" -ge 1 ]; then
     for i in "${SOURCE_EXTRA_FIRMWARES[@]}"
     do
         FIRMWARES+=( "$i" )
     done
 fi
+IFS=':' read -a TARGET_EXTRA_FIRMWARES <<< "$TARGET_EXTRA_FIRMWARES"
 if [ "${#TARGET_EXTRA_FIRMWARES[@]}" -ge 1 ]; then
     for i in "${TARGET_EXTRA_FIRMWARES[@]}"
     do

scripts/extract_fw.sh
@@ -202,12 +202,14 @@ EXTRACT_ALL()
 }
 
 FIRMWARES=( "$SOURCE_FIRMWARE" "$TARGET_FIRMWARE" )
+IFS=':' read -a SOURCE_EXTRA_FIRMWARES <<< "$SOURCE_EXTRA_FIRMWARES"
 if [ "${#SOURCE_EXTRA_FIRMWARES[@]}" -ge 1 ]; then
     for i in "${SOURCE_EXTRA_FIRMWARES[@]}"
     do
         FIRMWARES+=( "$i" )
     done
 fi
+IFS=':' read -a TARGET_EXTRA_FIRMWARES <<< "$TARGET_EXTRA_FIRMWARES"
 if [ "${#TARGET_EXTRA_FIRMWARES[@]}" -ge 1 ]; then
     for i in "${TARGET_EXTRA_FIRMWARES[@]}"
     do

scripts/internal/gen_config_file.sh
@@ -52,12 +52,7 @@ GEN_CONFIG_FILE()
         echo "ROM_CODENAME=\"${ROM_CODENAME:?}\""
         echo "SOURCE_FIRMWARE=\"${SOURCE_FIRMWARE:?}\""
         if [ "${#SOURCE_EXTRA_FIRMWARES[@]}" -ge 1 ]; then
-            echo -n "SOURCE_EXTRA_FIRMWARES=( "
-            for i in "${SOURCE_EXTRA_FIRMWARES[@]}"
-            do
-                echo -n "\"$i\" "
-            done
-            echo ")"
+            echo "SOURCE_EXTRA_FIRMWARES=\"$( IFS=:; printf '%s' "${SOURCE_EXTRA_FIRMWARES[*]}" )\""
         fi
         echo "SOURCE_API_LEVEL=\"${SOURCE_API_LEVEL:?}\""
         echo "SOURCE_VNDK_VERSION=\"${SOURCE_VNDK_VERSION:?}\""
@@ -65,12 +60,7 @@ GEN_CONFIG_FILE()
         echo "TARGET_CODENAME=\"${TARGET_CODENAME:?}\""
         echo "TARGET_FIRMWARE=\"${TARGET_FIRMWARE:?}\""
         if [ "${#TARGET_EXTRA_FIRMWARES[@]}" -ge 1 ]; then
-            echo -n "TARGET_EXTRA_FIRMWARES=( "
-            for i in "${TARGET_EXTRA_FIRMWARES[@]}"
-            do
-                echo -n "\"$i\" "
-            done
-            echo ")"
+            echo "TARGET_EXTRA_FIRMWARES=\"$( IFS=:; printf '%s' "${TARGET_EXTRA_FIRMWARES[*]}" )\""
         fi
         echo "TARGET_API_LEVEL=\"${TARGET_API_LEVEL:?}\""
         echo "TARGET_VNDK_VERSION=\"${TARGET_VNDK_VERSION:?}\""