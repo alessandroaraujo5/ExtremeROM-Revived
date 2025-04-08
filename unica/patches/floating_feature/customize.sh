alessandroaraujo5/ExtremeROM-Revived

commit 81d25fe18e3b28cbac95634e7e33ecd8c381dd9e

scripts: module_utils: fix up, minor adjustments
- Fix REMOVE_FROM_WORK_DIR behaviour when deleting file_context entries
- Fix SET_PROP behaviour when prop doesn't exists
- Fix SET_FLOATING_FEATURE_CONFIG/SET_PROP behaviour when deleting config/prop

Change-Id: I9c80f2d299f5af4b3e204640486f991d5e4951c3

unica/patches/floating_feature/customize.sh
@@ -9,10 +9,12 @@ READ_AND_APPLY_CONFIGS()
             [[ "$i" = "#"* ]] && continue
             [[ -z "$i" ]] && continue
 
-            if [[ "$i" == *"delete" ]] || [[ -z "$(echo -n "$i" | cut -d "=" -f 2)" ]]; then
-                SET_FLOATING_FEATURE_CONFIG "$(echo -n "$i" | cut -d " " -f 1)" --delete
-            elif echo -n "$i" | grep -q "="; then
-                SET_FLOATING_FEATURE_CONFIG "$(echo -n "$i" | cut -d "=" -f 1)" "$(echo -n "$i" | cut -d "=" -f2-)"
+            if echo -n "$i" | grep -q "="; then
+                if [[ -z "$(echo -n "$i" | cut -d "=" -f 2)" ]]; then
+                    SET_FLOATING_FEATURE_CONFIG "$(echo -n "$i" | cut -d "=" -f 1)" --delete
+                else
+                    SET_FLOATING_FEATURE_CONFIG "$(echo -n "$i" | cut -d "=" -f 1)" "$(echo -n "$i" | cut -d "=" -f 2)"
+                fi
             else
                 echo "Malformed string in target/$TARGET_CODENAME/sff.sh: \"$i\""
                 return 1

scripts/internal/apply_modules.sh
@@ -36,10 +36,12 @@ READ_AND_APPLY_PROPS()
             [[ "$i" = "#"* ]] && continue
             [[ -z "$i" ]] && continue
 
-            if [[ "$i" == *"delete" ]] || [[ -z "$(echo -n "$i" | cut -d "=" -f 2)" ]]; then
-                SET_PROP "$PARTITION" "$(echo -n "$i" | cut -d " " -f 1)" --delete
-            elif echo -n "$i" | grep -q "="; then
-                SET_PROP "$PARTITION" "$(echo -n "$i" | cut -d "=" -f 1)" "$(echo -n "$i" | cut -d "=" -f 2)"
+            if echo -n "$i" | grep -q "="; then
+                if [[ -z "$(echo -n "$i" | cut -d "=" -f 2)" ]]; then
+                    SET_PROP "$PARTITION" "$(echo -n "$i" | cut -d "=" -f 1)" --delete
+                else
+                    SET_PROP "$PARTITION" "$(echo -n "$i" | cut -d "=" -f 1)" "$(echo -n "$i" | cut -d "=" -f 2)"
+                fi
             else
                 echo "Malformed string in $patch: \"$i\""
                 return 1

scripts/utils/module_utils.sh
@@ -187,18 +187,20 @@ HEX_PATCH()
     TO="$(tr "[:upper:]" "[:lower:]" <<< "$TO")"
 
     if xxd -p "$FILE" | tr -d "\n" | tr -d " " | grep -q "$TO"; then
-        echo "\"$TO\" already applied in $(echo "$FILE" | sed "s.$WORK_DIR..")"
+        echo "\"$TO\" already applied in ${FILE//$WORK_DIR/}"
         return 0
     fi
 
     if ! xxd -p "$FILE" | tr -d "\n" | tr -d " " | grep -q "$FROM"; then
-        echo "No \"$FROM\" match in $(echo "$FILE" | sed "s.$WORK_DIR..")"
+        echo "No \"$FROM\" match in ${FILE//$WORK_DIR/}"
         return 1
     fi
 
-    echo "Patching \"$FROM\" to \"$TO\" in $(echo "$FILE" | sed "s.$WORK_DIR..")"
+    echo "Patching \"$FROM\" to \"$TO\" in ${FILE//$WORK_DIR/}"
     xxd -p "$FILE" | tr -d "\n" | tr -d " " | sed "s/$FROM/$TO/" | xxd -r -p > "$FILE.tmp"
     mv "$FILE.tmp" "$FILE"
+
+    return 0
 }
 
 # REMOVE_FROM_WORK_DIR "<partition>" "<file/dir>"
@@ -239,7 +241,7 @@ REMOVE_FROM_WORK_DIR()
     sed -i "$PATTERN" "$WORK_DIR/configs/fs_config-$PARTITION"
 
     PATTERN="${FILE//\//\\/}"
-    PATTERN="${PATTERN//\./\\\.}"
+    PATTERN="${PATTERN//\./\\\\\.}"
     if $IS_DIR; then
         PATTERN="/^\/$PATTERN/d"
     else
@@ -279,7 +281,7 @@ SET_FLOATING_FEATURE_CONFIG()
             echo "Replacing \"$CONFIG\" config with \"$VALUE\" in /system/system/etc/floating_feature.xml"
             sed -i "$(sed -n "/<${CONFIG}>/=" "$FILE") c\ \ \ \ <${CONFIG}>${VALUE}</${CONFIG}>" "$FILE"
         fi
-    else
+    elif [[ "$VALUE" != "-d" ]] && [[ "$VALUE" != "--delete" ]]; then
         echo "Adding \"$CONFIG\" config with \"$VALUE\" in /system/system/etc/floating_feature.xml"
         sed -i "/<\/SecFloatingFeatureSet>/d" "$FILE"
         if ! grep -q "Added by scripts" "$FILE"; then
@@ -306,16 +308,16 @@ SET_PROP()
         return 1
     fi
 
-    if GET_PROP "$PARTITION" "$PROP" &> /dev/null; then
+    if [[ $(GET_PROP "$PARTITION" "$PROP") ]]; then
         local FILES
         FILES="$(_GET_PROP_LOCATION "$PARTITION" "$PROP")"
         # shellcheck disable=SC2116
         for f in $(echo "$FILES"); do
-            if [[ "$PROP" == "-d" ]] || [[ "$PROP" == "--delete" ]]; then
-                echo "Deleting \"$PROP\" prop in $f" | sed "s.$WORK_DIR..g"
+            if [[ "$VALUE" == "-d" ]] || [[ "$VALUE" == "--delete" ]]; then
+                echo "Deleting \"$PROP\" prop in ${f//$WORK_DIR/}"
                 sed -i "/^$PROP/d" "$f"
             else
-                echo "Replacing \"$PROP\" prop with \"$VALUE\" in $f" | sed "s.$WORK_DIR..g"
+                echo "Replacing \"$PROP\" prop with \"$VALUE\" in ${f//$WORK_DIR/}"
 
                 local LINES
                 LINES="$(sed -n "/^${PROP}\b/=" "$f")"
@@ -324,7 +326,7 @@ SET_PROP()
                 done
             fi
         done
-    else
+    elif [[ "$VALUE" != "-d" ]] && [[ "$VALUE" != "--delete" ]]; then
         local FILE
 
         case "$PARTITION" in
@@ -339,21 +341,13 @@ SET_PROP()
                 fi
                 ;;
             "system_dlkm")
-                if [ -f "$WORK_DIR/system_dlkm/etc/build.prop" ]; then
-                    FILE="$WORK_DIR/system_dlkm/etc/build.prop"
-                else
-                    FILE="$WORK_DIR/system/system/system_dlkm/etc/build.prop"
-                fi
+                FILE="$WORK_DIR/system_dlkm/etc/build.prop"
                 ;;
             "vendor")
                 FILE="$WORK_DIR/vendor/build.prop"
                 ;;
             "vendor_dlkm")
-                if [ -f "$WORK_DIR/vendor_dlkm/etc/build.prop" ]; then
-                    FILE="$WORK_DIR/vendor_dlkm/etc/build.prop"
-                else
-                    FILE="$WORK_DIR/vendor/vendor_dlkm/etc/build.prop"
-                fi
+                FILE="$WORK_DIR/vendor_dlkm/etc/build.prop"
                 ;;
             "odm_dlkm")
                 FILE="$WORK_DIR/vendor/odm_dlkm/etc/build.prop"
@@ -371,7 +365,7 @@ SET_PROP()
             return 0
         fi
 
-        echo "Adding \"$PROP\" prop with \"$VALUE\" in $FILE" | sed "s.$WORK_DIR..g"
+        echo "Adding \"$PROP\" prop with \"$VALUE\" in ${FILE//$WORK_DIR/}"
         if ! grep -q "Added by scripts" "$FILE"; then
             echo "# Added by scripts/utils/module_utils.sh" >> "$FILE"
         fi

commit a751b24b92600738bada7d6e379276fd2630d03d

unica: patches: floating_feature: fix regression caused by 81d25fe1
Change-Id: I86c1519f81fc1d001cda89f86c2c215d8f8679f0

unica/patches/floating_feature/customize.sh
@@ -11,7 +11,7 @@ READ_AND_APPLY_CONFIGS()
                 if [[ -z "$(echo -n "$i" | cut -d "=" -f 2)" ]]; then
                     SET_FLOATING_FEATURE_CONFIG "$(echo -n "$i" | cut -d "=" -f 1)" --delete
                 else
-                    SET_FLOATING_FEATURE_CONFIG "$(echo -n "$i" | cut -d "=" -f 1)" "$(echo -n "$i" | cut -d "=" -f 2)"
+                    SET_FLOATING_FEATURE_CONFIG "$(echo -n "$i" | cut -d "=" -f 1)" "$(echo -n "$i" | cut -d "=" -f 2-)"
                 fi
             else
                 echo "Malformed string in target/$TARGET_CODENAME/sff.sh: \"$i\""