728c9dcd54ce54c28a4d896a089a556e551bd276
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-09-11 17:53:54 +0300

    [DNM] [WIP] platform: exynos990: adapt to r13s
    Change-Id: I31b691593d050c8259c07a1b6b3358a2ca1b7b22
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- platform/exynos990/patches/stock_blobs/customize.sh
+++ platform/exynos990/patches/stock_blobs/customize.sh
@@ -1,8 +1,8 @@
-# S24 FE OneUI 7 -> SoundBooster 2000
+# S25 FE OneUI 7 -> SoundBooster 2080
 # S20 Series -> SoundBooster 1050
 LOG_STEP_IN "- Replacing SoundBooster"
-DELETE_FROM_WORK_DIR "system" "system/lib64/lib_SoundBooster_ver2000.so"
-DELETE_FROM_WORK_DIR "system" "system/lib64/lib_SAG_EQ_ver2000.so"
+DELETE_FROM_WORK_DIR "system" "system/lib64/lib_SoundBooster_ver2080.so"
+DELETE_FROM_WORK_DIR "system" "system/lib64/lib_SAG_EQ_ver2080.so"
 DELETE_FROM_WORK_DIR "system" "system/lib64/libsoundboostereq_legacy.so"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/lib_SoundBooster_ver1050.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsamsungSoundbooster_plus_legacy.so" 0 0 644 "u:object_r:system_lib_file:s0"
@@ -14,8 +14,8 @@ ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/priv-app/DevGPUDriver-EX9830
 LOG_STEP_OUT
 
 LOG_STEP_IN "- Replacing Hotword"
-DELETE_FROM_WORK_DIR "product" "priv-app/HotwordEnrollmentOKGoogleEx4CORTEXM55"
-DELETE_FROM_WORK_DIR "product" "priv-app/HotwordEnrollmentXGoogleEx4CORTEXM55"
+DELETE_FROM_WORK_DIR "product" "priv-app/HotwordEnrollmentXGoogleEx6_WIDEBAND_LARGE"
+DELETE_FROM_WORK_DIR "product" "priv-app/HotwordEnrollmentYGoogleEx6_WIDEBAND_LARGE"
 if [[ "$TARGET_CODENAME" != "r8s" ]]; then
     ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "product" "priv-app/HotwordEnrollmentOKGoogleEx3CORTEXM4/HotwordEnrollmentOKGoogleEx3CORTEXM4.apk" 0 0 644 "u:object_r:system_file:s0"
     ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "product" "priv-app/HotwordEnrollmentXGoogleEx3CORTEXM4/HotwordEnrollmentXGoogleEx3CORTEXM4.apk" 0 0 644 "u:object_r:system_file:s0"
