0f4c31c97d90248eb81060b49b7fc62b68fb1c9b
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-09-12 17:26:37 +0300

    unica: patches: uwb: fixup
    Change-Id: Ia98badaeb8534712041de8420e4830204ef8ed4a
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- unica/patches/uwb/customize.sh
+++ unica/patches/uwb/customize.sh
@@ -4,7 +4,6 @@ REGION=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 2)
 if [ -d "$FW_DIR/${MODEL}_${REGION}/vendor/firmware/uwb" ]; then
     LOG_STEP_IN "- Adding S24+ (e2sxxx) UWB blobs"
     ADD_TO_WORK_DIR "e2sxxx" "system" "system/app/UwbTest"
-    ADD_TO_WORK_DIR "e2sxxx" "system" "system/app/UwbRROverlayMccMncRegulation"
     ADD_TO_WORK_DIR "e2sxxx" "system" "system/etc/init/init.system.uwb.rc"
     ADD_TO_WORK_DIR "e2sxxx" "system" "system/etc/permissions/com.samsung.android.uwb_extras.xml"
     ADD_TO_WORK_DIR "e2sxxx" "system" "system/framework/com.samsung.android.uwb_extras.jar"
