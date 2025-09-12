2b760d81417bd5c9cc95e4123bdda1d2b3538613
xfwdrev <74693315+xfwdrev@users.noreply.github.com>
2025-09-13 02:09:26 +0800

    platform: 9820/990/2100: Fix typos
    Change-Id: I6f91017d9a088f6641d388f2f515efd9b7d0ed7e
    Signed-off-by: xfwdrev <74693315+xfwdrev@users.noreply.github.com>
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>
    Signed-off-by: dupazlasu <dupazlasu66@gmail.com>

--- platform/exynos990/patches/midas/customize.sh
+++ platform/exynos990/patches/midas/customize.sh
@@ -1,4 +1,4 @@
-LOG_STEP_IN "- Adding S21 (r9sxxx) Ultra MIDAS"
+LOG_STEP_IN "- Adding S21 FE (r9sxxx) MIDAS"
 DELETE_FROM_WORK_DIR "vendor" "etc/midas"
 DELETE_FROM_WORK_DIR "vendor" "etc/VslMesDetector"
 ADD_TO_WORK_DIR "r9sxxx" "vendor" "etc/midas"
@@ -9,12 +9,12 @@ LOG "- Fixing MIDAS model detection"
 sed -i "s/$SOURCE_CODENAME/dummy/g" "$WORK_DIR/vendor/etc/midas/midas_config.json"
 sed -i "s/r9s/$SOURCE_CODENAME/g" "$WORK_DIR/vendor/etc/midas/midas_config.json"
 
-LOG_STEP_IN "- Adding S21 (r9sxxx) Photo Remaster Service"
+LOG_STEP_IN "- Adding S21 FE (r9sxxx) Photo Remaster Service"
 DELETE_FROM_WORK_DIR "system" "system/priv-app/PhotoRemasterService/oat"
 ADD_TO_WORK_DIR "r9sxxx" "system" "system/priv-app/PhotoRemasterService/PhotoRemasterService.apk"
 LOG_STEP_OUT
 
-LOG_STEP_IN "- Adding S21 (r9sxxx) MIDAS libraries"
+LOG_STEP_IN "- Adding S21 FE (r9sxxx) MIDAS libraries"
 ADD_TO_WORK_DIR "r9sxxx" "system" "system/lib64/libmidas_core.camera.samsung.so"
 ADD_TO_WORK_DIR "r9sxxx" "system" "system/lib64/libmidas_DNNInterface.camera.samsung.so"
 LOG_STEP_OUT
