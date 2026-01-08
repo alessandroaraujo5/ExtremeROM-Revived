6e87060ddf04f99a19420ec32aa01fd71b4ba6f6
Android-Artisan <romartisan2025@gmail.com>
2026-01-08 21:45:53 +0400

    unica: configs: version.sh:  update version nameing and remove rom+

--- unica/configs/version.sh
+++ unica/configs/version.sh
@@ -17,10 +17,10 @@
 
 VERSION_MAJOR=3
 VERSION_MINOR=0
-VERSION_PATCH=0-alpha1
+VERSION_PATCH=0
 
 ROM_VERSION="${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}"
 # Append current commit hash to version name
-ROM_VERSION+="-$(git rev-parse --short HEAD)"
+ROM_VERSION+=" "
 
 ROM_CODENAME="Quant"
